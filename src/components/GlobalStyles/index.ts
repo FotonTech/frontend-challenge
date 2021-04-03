a95562aaf3b52d203c253230f2abd937d7ae5e5a
hebit <temp.personal.mail@protonmail.com>
2021-04-03 17:55:57 -0300

    fix: create global style component

---
+++ src/components/GlobalStyles/index.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle` 
+  body {
+    background-color: #fef000;
+    font-family: 'Rubik', sans-serif;
+  }  
+`;
