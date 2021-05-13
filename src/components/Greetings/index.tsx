0dd98836b5c2e59da218f309d88a482dfa2a7c27
Raul Rosa <raulvictorrosa@gmail.com>
2021-05-12 22:36:10 -0300

    feat: component Greetings

---
+++ src/components/Greetings/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { H4 } from './styled';
+
+type GreetingsTypes = React.FC<{
+  name: string;
+}>;
+
+const SearchField: GreetingsTypes = ({ name }) => {
+  return (
+    <H4>
+      <span>Hi, </span>
+      {`${name}`}
+      <span role="img" aria-label="Waving">
+        👋
+      </span>
+    </H4>
+  );
+};
+
+export default SearchField;
---
+++ src/components/Greetings/styled.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const H4 = styled.h4`
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 29px;
+  letter-spacing: -0.6px;
+
+  span {
+    color: #54565a;
+    font-weight: normal;
+    &[role='img'] {
+      margin-left: 10px;
+    }
+  }
+`;
