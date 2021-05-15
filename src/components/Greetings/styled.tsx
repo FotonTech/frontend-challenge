0922308b8f1da6248e579fa4eed3d9059719c1b7
Raul Rosa <raulvictorrosa@gmail.com>
2021-05-15 14:52:55 -0300

    feat: change style of component greeting

--- src/components/Greetings/styled.tsx
+++ src/components/Greetings/styled.tsx
@@ -4,11 +4,14 @@ export const H4 = styled.h4`
   font-weight: 600;
   font-size: 24px;
   line-height: 29px;
-  letter-spacing: -0.6px;
+  letter-spacing: -0.7px;
+  margin-bottom: 30px;
+  color: #ff6978;
 
   span {
     color: #54565a;
     font-weight: normal;
+    letter-spacing: 0;
     &[role='img'] {
       margin-left: 10px;
     }
