5f238b5bce11deee9b6bec4d9a91dcdf1756b99e
Raul Rosa <raulvictorrosa@gmail.com>
2021-05-15 14:52:24 -0300

    feat: component currently reading

---
+++ src/components/CurrentlyReading/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+  Col,
+  Row
+} from 'reactstrap';
+import OvalTopPng from '../../images/oval-top-card-reading.png';
+import BlockWrapper from '../BlockWrapper';
+import {
+  BookIcon,
+  Card,
+  CardImg,
+  Circle,
+  OvalImg,
+  Rectangle,
+  Wrapper
+} from './styled';
+
+const DiscoverBooks: React.FC = () => {
+  return (
+    <BlockWrapper title="Currently Reading" linkTitle="All" href="#">
+      <Card>
+        <Row className="no-gutters">
+          <Col>
+            <CardBody>
+              <CardImg
+                src="http://books.google.com/books/content?id=dsz5AwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api"
+                height="130"
+                width="88"
+              />
+              <div className="wrap-figures">
+                <Circle />
+                <OvalImg
+                  className="oval-top"
+                  src={OvalTopPng}
+                  width="69"
+                  height="69"
+                />
+                <Wrapper>
+                  <CardTitle tag="h5">Originals</CardTitle>
+                  <CardSubtitle tag="h6">by Adam Grant</CardSubtitle>
+                  <CardText>
+                    <BookIcon size="16px" />
+                    Chapter <span>2</span> From 9
+                  </CardText>
+                </Wrapper>
+                <Rectangle />
+              </div>
+            </CardBody>
+          </Col>
+        </Row>
+      </Card>
+    </BlockWrapper>
+  );
+};
+
+export default DiscoverBooks;
---
+++ src/components/CurrentlyReading/styled.tsx
@@ -0,0 +1,123 @@
+import { Card as CardStyled, CardImg as CardImgStyled } from 'reactstrap';
+import styled from 'styled-components';
+import { ReactComponent as BookIconSvg } from '../../images/book-icon.svg';
+
+type SvgIconType = {
+  size: string;
+};
+
+export const Wrapper = styled.div`
+  padding-left: calc(var(--card-img-width) + 10px);
+  padding-top: 10px;
+  padding-bottom: 9px;
+`;
+
+export const Card = styled(CardStyled)`
+  width: 100%;
+  max-width: 331px;
+  background: transparent;
+  border-radius: 0;
+  border: none;
+  left: -19px;
+  --card-img-width: 88px;
+  --card-img-height: 130px;
+  height: var(--card-img-height);
+  justify-content: center;
+
+  .card {
+    &-body {
+      border-radius: 0px 3px 3px 0px;
+      box-shadow: 0px 3px 45px rgba(121, 141, 67, 0.115084);
+      background: #eef5db;
+      padding: 0;
+      padding-left: 20px;
+      position: relative;
+      z-index: 1;
+    }
+
+    &-img {
+      border-radius: 1px;
+      border-radius: 0;
+      position: absolute;
+      z-index: 1;
+      top: -15px;
+    }
+
+    &-title {
+      font-family: 'Playfair Display', sans-serif;
+      font-weight: bold;
+      line-height: 27px;
+      letter-spacing: 2px;
+      margin-bottom: 5px;
+      color: #2a2b26;
+    }
+
+    &-subtitle {
+      font-family: 'Roboto', sans-serif;
+      font-size: 10px;
+      line-height: 12px;
+      letter-spacing: -0.1px;
+      margin-top: 0;
+      margin-bottom: 21px;
+      color: #74776d;
+    }
+
+    &-text {
+      font-size: 10px;
+      line-height: 12px;
+      letter-spacing: -0.05px;
+      color: #2a2b26;
+
+      span {
+        font-weight: 600;
+        color: #ff6978;
+      }
+    }
+  }
+
+  .wrap-figures {
+    overflow: hidden;
+    position: relative;
+  }
+
+  .oval-top {
+    position: absolute;
+    top: -19px;
+    right: -34px;
+  }
+`;
+
+export const CardImg = styled(CardImgStyled)`
+  width: var(--card-img-width);
+  height: var(--card-img-height);
+`;
+
+export const OvalImg = styled.img``;
+
+export const Circle = styled.div`
+  width: 20px;
+  height: 20px;
+  border: 2px solid #4550a7;
+  border-radius: 100%;
+  position: absolute;
+  top: -7px;
+  right: 89px;
+`;
+
+export const Rectangle = styled.div`
+  width: 46px;
+  height: 4px;
+  background: #ec6374;
+  transform: rotate(14deg);
+  position: absolute;
+  bottom: 26px;
+  right: -23px;
+`;
+
+export const BookIcon = styled(BookIconSvg)`
+  width: ${({ size }: SvgIconType) => size};
+  height: ${({ size }: SvgIconType) => size};
+  position: relative;
+  top: -1px;
+  margin-right: 3px;
+`;
