59fe2776a45903cd44e74ecf56e70319c32e2b6c
Raul Rosa <raulvictorrosa@gmail.com>
2021-05-18 10:03:20 -0300

    feat: improve the verification

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -24,7 +24,7 @@ const Home: React.FC = () => {
         </Row>
         {isLoading && <>Is Loading</>}
         {books.items.length > 0 && !isLoading && search !== '' && <BooksList />}
-        {books.items.length == 0 && !isLoading && search === '' && (
+        {books.items.length === 0 && !isLoading && search === '' && (
           <>
             <Row>
               <Col>
