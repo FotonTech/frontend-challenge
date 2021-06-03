843d16966acbf51f1a5eab684aa3b9946eafe927
mb4ndeira <mbandeira.cs@gmail.com>
2021-06-03 12:41:59 -0300

    feat: add fonts preconnect

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,11 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="preload" href="fonts/SFProText/SF-Pro-Text-Regular.otf" as="font" crossOrigin="" />
+                    <link rel="preload" href="fonts/SFProText/SF-Pro-Text-Bold.otf" as="font" crossOrigin="" />
+                    <link rel="preload" href="fonts/SFProDisplay/SF-Pro-Display-Regular.otf" as="font" crossOrigin="" />
+                    <link rel="preload" href="fonts/SFProDisplay/SF-Pro-Display-Semibold.otf" as="font" crossOrigin="" />
+                    <link rel="preload" href="fonts/SFProDisplay/SF-Pro-Display-Bold.otf" as="font" crossOrigin="" />
                     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap" rel="stylesheet" />
                 </Head>
