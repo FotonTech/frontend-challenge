cc812884b5473154b382e0172bf0d21c6e534ba6
mb4ndeira <mbandeira.cs@gmail.com>
2021-06-04 08:22:32 -0300

    Revert "feat add home 'hello'"
    This reverts commit d2594bd42d9c5379253c65a9f7e516e6aad7264d.

--- src/components/Paint/index.tsx
+++ src/components/Paint/index.tsx
@@ -8,7 +8,7 @@ interface PaintProps {
     radius?: string;
 }
 
-export default function Paint({ color, height = 190, radius = `${String((height / 100) * 17.5)}px` }: PaintProps) {
+export default function Paint({ color, height = 150, radius = `${String((height / 100) * 17.5)}px` }: PaintProps) {
     return (
         <div className={styles.paint} style={{ backgroundColor: color, height, borderRadius: `0 0 ${radius} 0` }} />
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,10 @@ import NavBar from '../components/NavBar'
 import styles from './home.module.scss'
 
 export default function Home() {
-  const key = 'AIzaSyCMe1GHh0PynH7AvrTt4h2nq0m-vZ40nuo'
-  const user = '100881681654827357753'
-  fetch(``)
-    .then(response => response.json())
-    .then(data => console.log(data));
-
   return (
     <main className={styles.main}>
       <Paint color='#f2f2f2' />
       <SearchBar />
-      <p className={styles.hello}>Hi, <strong>Mehmed Al Fatih</strong> 👋</p>
       <NavBar />
     </main>
   )
