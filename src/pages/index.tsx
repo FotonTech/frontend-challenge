671ae0770f4cb3864526c857cce653ac2e95c4b8
mb4ndeira <mbandeira.cs@gmail.com>
2021-06-04 08:29:50 -0300

    feat: add home 'hello'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,16 @@ import NavBar from '../components/NavBar'
 import styles from './home.module.scss'
 
 export default function Home() {
+  const user = '100881681654827357753'
+  fetch('')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
   return (
     <main className={styles.main}>
       <Paint color='#f2f2f2' />
       <SearchBar />
+      <p className={styles.hello}>Hi, <strong>Mehmed Al Fatih</strong> 👋</p>
       <NavBar />
     </main>
   )
