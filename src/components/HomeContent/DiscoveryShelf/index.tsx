08fc8a11121b98deede1f4515ddd00f7e2956ce2
mb4ndeira <mbandeira.cs@gmail.com>
2021-06-08 11:14:58 -0300

    fix: refactor home content components

---
+++ src/components/HomeContent/DiscoveryShelf/index.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+import Book from '../../../types/Book'
+
+import styles from './discovery-shelf.module.scss'
+
+interface DiscoveryShelfProps {
+    title: string
+    books: Book[]
+}
+
+export default function DiscoveryShelf({ title, books }: DiscoveryShelfProps) {
+    return (
+        <div className={styles.discoveryShelf}>
+            <h2>{title}</h2>
+            <div>
+                {books.map(book => {
+                    return (
+                        <Link href="#" key={book.title}>
+                            <div>
+                                <section>
+                                    <div>
+                                        <h3>{book.title}</h3>
+                                        <h4>{book.authors[0]}</h4>
+                                    </div>
+                                    <span><img src="icons/statistics.svg"></img> <strong>120+</strong> Read Now</span>
+                                </section>
+                                <img src={book.imageLinks.thumbnail} alt="bookCover" />
+                            </div>
+                        </Link>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import Book from '../../types/Book'
 import Bookshelf from '../../types/Bookshelf'
 
+import DiscoveryShelf from './DiscoveryShelf'
+
 import styles from './home-content.module.scss'
 
 interface HomeContentProps {
@@ -12,38 +14,18 @@ interface HomeContentProps {
 }
 
 export default function HomeContent({ discoveries, reading, books }: HomeContentProps) {
-    let discoverShelf: Book[] = []
+    let discoveryShelf: Book[] = []
     let readingShelf: Book[] = []
 
     if (discoveries)
-        discoverShelf = discoveries.books.map(item => books.find(book => item.id === book.id))
+        discoveryShelf = discoveries.books.map(item => books.find(book => item.id === book.id))
 
     if (reading)
         readingShelf = reading.books.map(item => books.find(book => item.id === book.id))
 
     return (
         <div className={styles.homeContent}>
-            <div className={styles.discoveries}>
-                <h2>{discoveries?.title}</h2>
-                <div>
-                    {discoverShelf.map(book => {
-                        return (
-                            <Link href="#" key={book.title}>
-                                <div>
-                                    <section>
-                                        <div>
-                                            <h3>{book.title}</h3>
-                                            <h4>{book.authors[0]}</h4>
-                                        </div>
-                                        <span><img src="icons/statistics.svg"></img> <strong>120+</strong> Read Now</span>
-                                    </section>
-                                    <img src={book.imageLinks.thumbnail} alt="bookCover" />
-                                </div>
-                            </Link>
-                        )
-                    })}
-                </div>
-            </div>
+            <DiscoveryShelf title={discoveries?.title} books={discoveryShelf} />
         </div>
     )
 }
