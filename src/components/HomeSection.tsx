f57cdfc0a5f9ea9a9bec880b7e9452d6d44b1479
Breno Salvador de Freitas <brenosalvadordefreitas@gmail.com>
2021-06-16 21:37:28 -0300

    Add 'Reviews of The Day' section to Home page

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,15 +1,16 @@
-import { FC } from "react";
+import { CSSProperties, FC } from "react";
 
 import styles from "../styles/components/HomeSection.module.css";
 
 interface HomeSectionProps {
   sectionLabel: string;
   linkLabel: string;
+  style?: CSSProperties;
 }
 
 export const HomeSection: FC<HomeSectionProps> = (props) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={props.style}>
       <header className={styles.header}>
         <p>{props.sectionLabel}</p>
         <p>{props.linkLabel}</p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -78,17 +78,7 @@ function Home() {
         <SearchBox placeholder="Search book" />
       </header>
 
-      <svg className={styles.oval1} width="80" height="127" viewBox="0 0 80 127" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-        <circle cx="63.5" cy="63.5" r="63.5" fill="url(#pattern0)" />
-        <defs>
-          <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="0.319742" height="0.056425">
-            <use xlinkHref="#image0" transform="scale(0.00940417)" />
-          </pattern>
-          <image id="image0" width="34" height="6" xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAGCAYAAACmekziAAAABGdBTUEAALGOfPtRkwAAAY5JREFUKBW9Uj1IQlEUPuf6pCQofI9+CLMhaynInwyKfrYgGgxRCyEICpyaImh0aomaJKjFagjhUVRDRJNNDv61tLokgRROiZV6T/cFwitsKjpwOYfvnPN9H/deA/xjdM4stTS19W81dw+VN0OBfDwep7o81gt9DofDTByux35bK3afh4BHAPGMgKYByIKElwjswmTCmy9G2p1+G6fqOgHaEPGItbLzp7j68pMJ2+xaU7Hw6AGiVTFTIYQ7JMhKzJgtpGI5wUFml98KvBZBogGGUug5q95qfBrOeHVeaGn77k8jst03LtxuANIkAu4hwwyv8RVEmALAK4Z40ovKdTp9UNFIlBH/KK/VloFgQcxkBLSPxCqCw05IDrHjEMJmIZISvWHGcLerj23fq+q7tv89rBNBM8oOb4IIOhjAjslkiOYTark+aBnzy6VXHgDgQSAcFKJR0ZsTDysJc4dkMB4Xk7GH+rw+a+Sl0ptLkiBXSJ7m9L2GteL0ebU/0bCpA2X3Yo/s9Ia129PBf1Z+AO3ViTcdfWOLAAAAAElFTkSuQmCC" />
-        </defs>
-      </svg>
-
-      <main>
+      <main className={styles.main}>
         <div className={styles.welcomeMessage}>
           <p>Hi,&nbsp;</p>
           <p>{userName}</p>
@@ -101,6 +91,16 @@ function Home() {
         </div>
 
         <HomeSection sectionLabel="Discover new book" linkLabel="More">
+          <svg className={styles.oval1} width="80" height="127" viewBox="0 0 80 127" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+            <circle cx="63.5" cy="63.5" r="63.5" fill="url(#pattern0)" />
+            <defs>
+              <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="0.319742" height="0.056425">
+                <use xlinkHref="#image0" transform="scale(0.00940417)" />
+              </pattern>
+              <image id="image0" width="34" height="6" xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAGCAYAAACmekziAAAABGdBTUEAALGOfPtRkwAAAY5JREFUKBW9Uj1IQlEUPuf6pCQofI9+CLMhaynInwyKfrYgGgxRCyEICpyaImh0aomaJKjFagjhUVRDRJNNDv61tLokgRROiZV6T/cFwitsKjpwOYfvnPN9H/deA/xjdM4stTS19W81dw+VN0OBfDwep7o81gt9DofDTByux35bK3afh4BHAPGMgKYByIKElwjswmTCmy9G2p1+G6fqOgHaEPGItbLzp7j68pMJ2+xaU7Hw6AGiVTFTIYQ7JMhKzJgtpGI5wUFml98KvBZBogGGUug5q95qfBrOeHVeaGn77k8jst03LtxuANIkAu4hwwyv8RVEmALAK4Z40ovKdTp9UNFIlBH/KK/VloFgQcxkBLSPxCqCw05IDrHjEMJmIZISvWHGcLerj23fq+q7tv89rBNBM8oOb4IIOhjAjslkiOYTark+aBnzy6VXHgDgQSAcFKJR0ZsTDysJc4dkMB4Xk7GH+rw+a+Sl0ptLkiBXSJ7m9L2GteL0ebU/0bCpA2X3Yo/s9Ia129PBf1Z+AO3ViTcdfWOLAAAAAElFTkSuQmCC" />
+            </defs>
+          </svg>
+
           <SwipeableViews
             style={{
               width: "80vw",
@@ -203,9 +203,15 @@ function Home() {
                 <image id="image0" width="34" height="6" xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAGCAYAAACmekziAAAABGdBTUEAALGOfPtRkwAAAY5JREFUKBW9Uj1IQlEUPuf6pCQofI9+CLMhaynInwyKfrYgGgxRCyEICpyaImh0aomaJKjFagjhUVRDRJNNDv61tLokgRROiZV6T/cFwitsKjpwOYfvnPN9H/deA/xjdM4stTS19W81dw+VN0OBfDwep7o81gt9DofDTByux35bK3afh4BHAPGMgKYByIKElwjswmTCmy9G2p1+G6fqOgHaEPGItbLzp7j68pMJ2+xaU7Hw6AGiVTFTIYQ7JMhKzJgtpGI5wUFml98KvBZBogGGUug5q95qfBrOeHVeaGn77k8jst03LtxuANIkAu4hwwyv8RVEmALAK4Z40ovKdTp9UNFIlBH/KK/VloFgQcxkBLSPxCqCw05IDrHjEMJmIZISvWHGcLerj23fq+q7tv89rBNBM8oOb4IIOhjAjslkiOYTark+aBnzy6VXHgDgQSAcFKJR0ZsTDysJc4dkMB4Xk7GH+rw+a+Sl0ptLkiBXSJ7m9L2GteL0ebU/0bCpA2X3Yo/s9Ia129PBf1Z+AO3ViTcdfWOLAAAAAElFTkSuQmCC" />
               </defs>
             </svg>
-
-
           </div>
+        </HomeSection>
+
+        <HomeSection
+          sectionLabel="Reviews of The Days"
+          linkLabel="All Video"
+          style={{ marginTop: "45px" }}
+        >
+          <img className={styles.videoThumbnail} src="video-thumbnail.png" alt="Video thumbnail" />
         </HomeSection>
       </main>
 
