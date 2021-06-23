ca7892dc0f46b58b84bf410d717815cf286c5b75
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-23 19:02:08 -0300

    99.9%

--- google-books/src/components/DiscoverCarousel/index.js
+++ google-books/src/components/DiscoverCarousel/index.js
@@ -5,10 +5,6 @@ import CarouselCard from './CarouselCard';
 import BookCover1 from '../../imgs/BookCover1.png';
 import BookCover2 from '../../imgs/book2.png';
 import fetchBook from '../../services';
-import triangle from '../../imgs/home/effects/triangle.png';
-import rectangle from '../../imgs/home/effects/Rectangle.png';
-import circle from '../../imgs/home/effects/transparent_circle.png';
-import oval from '../../imgs/home/effects/Oval.png';
 
 export default function DiscoverCarousel() {
   const history = useHistory();
@@ -68,11 +64,6 @@ export default function DiscoverCarousel() {
           />
         </button>
       </div>
-      <img src={triangle} alt="effects" style={{ position: 'absolute', top: '218px', left: '173px' }} />
-      <img src={rectangle} alt="effects" style={{ position: 'absolute', top: '315px', left: '150px' }} />
-      <img src={circle} alt="effects" style={{ position: 'absolute', top: '225px', left: '150px' }} />
-      <img src={oval} alt="effects" style={{ position: 'absolute', top: '245px', left: '-10px' }} />
-      <img src={oval} alt="effects" style={{ position: 'absolute', top: '170px', left: '275px' }} />
     </DiscoverCarouselDisplay>
   );
 }
