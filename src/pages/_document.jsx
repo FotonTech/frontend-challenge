83ea3ab565fd940de6508682c2a886cd6c75433f
yuribmontez <ybmontez@gmail.com>
2021-09-22 16:15:15 -0300

    feat: add foton favicon

---
+++ src/pages/_document.jsx
@@ -0,0 +1,17 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/foton-favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
