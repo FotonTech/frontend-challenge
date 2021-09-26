55d7baac348f2af103726fa93eb104c1cf6efe89
yuribmontez <ybmontez@gmail.com>
2021-09-26 19:44:55 -0300

    feat: roboto font, foton favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,6 +5,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
           <link rel="shortcut icon" href="/foton-favicon.png" type="image/png" />
         </Head>
         <body>
