f37b86ad74907778611847bf24d2afd7ae15dc55
yuribmontez <ybmontez@gmail.com>
2021-09-26 19:43:48 -0300

    feat: custom chakra theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,24 +2,38 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
   colors: {
-    textHeader: "#3F4043",
-    textPrimary: "#313131",
-    textSecondary: "#54565A",
-    textHighlight: "#FF6978",
-    textBlue: '#4ABDF1',
-    background: "#FEFAF6",
-    white: '#FFF',
+    background: '#FEFAF6',
+    highlight: "#FF6978",
+    link: '#4ABDF1',
     black: '#000',
+    white: {
+      100: '#FEFEFE',
+      300: '#E7E7E1'
+    },
+    text: {
+      300: '#C8C8C8',
+      500: '#54565A',
+      600: '#3F4043',
+      700: '#313131',
+      800: '#74776D',
+    },
+    title: {
+      500: '#36383A',
+      700: '#2A2B26',
+    },
   },
   fonts: {
-    heading: 'SFProDisplay',
-    body: 'SFProDisplay'
+    heading: `SFProDisplay, Roboto, sans-serif`,
+    body: `SFProDisplay, Roboto, sans-serif`,
   },
   styles: {
     global: {
       body: {
         bg: 'background',
-        color: 'textSecondary',
+        color: 'text.600',
+      },
+      a: {
+        cursor: 'pointer'
       }
     }
   }
