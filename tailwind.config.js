947e50ed73b1731985d053e8f0cecadce9234917
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-09-30 23:46:47 +0200

    chore: configure and add app colors

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      "color-placeholder": "#54565A",
+      "color-text-light": "rgba(49, 49, 49, 0.6);",
+      "color-text-dark": "rgba(49, 49, 49, 0.8);",
+      title: "#3F4043",
+      pink: "#FF6978",
+      blue: "#4ABDF1",
+      gray: "#BFBEBF",
+    },
+    backgroundColor: {
+      primary: "#f2f2f2",
+      white: "#fff",
+      "dark-blue": "#00173D",
+      purple: "#451475",
+      "light-green": "#EEF5DB",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["SFPro", ...fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
