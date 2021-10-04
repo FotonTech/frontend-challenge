5cb2b3a83a1295d889312268fea49a2408d16470
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-10-04 11:30:54 +0200

    style(Input): add background; border radius

--- src/components/InputSearch/index.js
+++ src/components/InputSearch/index.js
@@ -1,8 +1,14 @@
+import Image from "next/image";
+import SearchIcon from "../../../public/images/search.svg";
+
 export const InputSearch = (props) => {
   return (
-    <input
-      className="rounded-[10px] py-[15px] px-[20px] w-full text-black outline-none text-base placeholder-color-placeholder"
-      {...props}
-    />
+    <div className="flex gap-[10px] bg-cloud rounded-[10px] py-[15px] px-[20px]">
+      <Image src={SearchIcon} alt="search icon" />
+      <input
+        className="w-full bg-cloud text-black outline-none text-base placeholder-color-placeholder"
+        {...props}
+      />
+    </div>
   );
 };
