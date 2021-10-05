e3a788e58675a1ca1bb061518f95ba4052ef7f49
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-10-05 18:28:10 +0200

    feat: create book list and details components

---
+++ src/pages/books/[id].js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BookOpository</title>
+      </Head>
+
+      <div>Books details page comes here</div>
+    </>
+  );
+}
---
+++ src/pages/books/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BookOpository</title>
+      </Head>
+
+      <div>Books page comes here</div>
+    </>
+  );
+}
