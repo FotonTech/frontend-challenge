47188bdde42c5251b949cac5a260691d59c48fa9
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-10-05 18:27:29 +0200

    feat: add cards sections component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import { CardDiscover, CardReading, CardReview } from "components/Cards";
+import { SectionCard } from "components/Cards/SectionCard";
+import { Greeting } from "components/Greetings";
 import { InputSearch } from "components/InputSearch";
 import { Navbar } from "components/Navbar";
 import Head from "next/head";
@@ -10,11 +13,29 @@ export default function Home() {
       </Head>
 
       <div>
-        <div className="fixed bottom-0 left-0 right-0">
+        <div className="fixed z-10 bottom-0 left-0 right-0">
           <Navbar />
         </div>
-
         <InputSearch placeholder="Search book" />
+        <Greeting name="Adilson Mandlate" />
+        <SectionCard
+          title="Discover new book"
+          titleAction={{ name: "More", link: "/more" }}
+        >
+          <CardDiscover />
+        </SectionCard>
+        <SectionCard
+          title="Currently Reading"
+          titleAction={{ name: "All", link: "/more" }}
+        >
+          <CardReading />
+        </SectionCard>
+        <SectionCard
+          title="Reviews of The Days"
+          titleAction={{ name: "All video", link: "/more" }}
+        >
+          <CardReview />
+        </SectionCard>
       </div>
     </>
   );
