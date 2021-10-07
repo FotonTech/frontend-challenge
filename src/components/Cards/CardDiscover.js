876df25ba14fb3464b4ab739f64a7a9dc8cea8f3
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-10-07 15:29:40 +0200

    feat: add card components

--- src/components/Cards/CardDiscover.js
+++ src/components/Cards/CardDiscover.js
@@ -1,27 +1,54 @@
 import Image from "next/image";
+import Link from "next/link";
 import sticks from "../../../public/images/sticks.svg";
 
-export const CardDiscover = ({ book }) => {
+export const CardDiscover = ({ book, index, swiperIndex }) => {
+  const isCurrentIndex = index === swiperIndex;
+  const widthCard = isCurrentIndex ? "272px" : "250px";
+  const heightCard = isCurrentIndex ? "139px" : "128px";
+
   return (
-    <div className="rounded-[5px] w-[272px] h-[139px] py-[15px] px-[20px] bg-dark-blue text-gray flex bg-circle bg-no-repeat bg-left-bottom">
-      <div className="flex flex-1 flex-col justify-between">
-        <div>
-          <h3 className="text-[27px] font-bold font-serif text-cloud">
-            Titulo
-          </h3>
-          <h5 className="text-sm italic">Autor</h5>
-        </div>
-        <p className="text-[10px] text-card-gray">Reading</p>
-      </div>
-      <div className="relative">
-        <div
-          className="absolute top-[-11px] left-[-36px] z-10"
-          role="background"
-        >
-          <Image src={sticks} alt="sticks foreground" />
-        </div>
-        <span>Image</span>
-      </div>
+    <div
+      className={`rounded-[5px] py-[15px] px-[20px] transition-all ${
+        isCurrentIndex ? "bg-dark-blue" : "bg-purple"
+      } duration-150 mr-[10px] text-gray bg-circle bg-no-repeat bg-left-bottom`}
+      style={{
+        width: widthCard,
+        height: heightCard,
+      }}
+    >
+      <Link href={`/books/${book.id}`}>
+        <a className="flex">
+          <div className="flex flex-1 flex-col justify-between">
+            <div>
+              <h3 className="font-bold font-serif text-cloud">{book.title}</h3>
+              <h5 className="text-sm italic">
+                {book.authors.map((author) => `${author},`)}
+              </h5>
+            </div>
+            <p className="text-[10px] text-card-gray">120+ Read now</p>
+          </div>
+          <div className="relative">
+            <div
+              className="absolute top-[-11px] left-[-36px] z-10"
+              role="background"
+            >
+              <Image
+                src={sticks}
+                width={59}
+                height={108}
+                alt="sticks foreground"
+              />
+            </div>
+            <Image
+              width={72}
+              height={111}
+              src={book.image}
+              alt="book thumbnail"
+            />
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
--- src/components/Cards/CardReading.js
+++ src/components/Cards/CardReading.js
@@ -1,24 +1,26 @@
 import Image from "next/image";
-import bookImage from "../../../public/images/bookCover.svg";
+import Link from "next/link";
 
 export const CardReading = ({ book }) => {
   return (
-    <div className="rounded-[3px] mt-[30px] w-[331px] h-[100px] py-[10px] px-[20px] bg-light-green text-black flex gap-2 bg-currentlyReading bg-minus-top-right bg-no-repeat">
+    <div className="rounded-[3px] my-[18px] w-[331px] h-[100px] py-[10px] px-[20px] flex gap-2 bg-light-green text-black  bg-currentlyReading bg-minus-top-right bg-no-repeat">
       <div className="mt-[-32px]">
         <Image
           width={90}
           height={136}
-          src={bookImage}
-          alt="currently reading book image"
+          src={book.image}
+          alt="currently reading book thumbnail"
         />
       </div>
       <div className="flex flex-1 flex-col justify-between">
         <div>
-          <h3 className="text-[20px] font-bold font-serif">Titulo</h3>
-          <h5 className="text-sm italic">Autor</h5>
+          <h3 className="text-[16px] font-bold font-serif">{book.title}</h3>
+          <h5 className="text-sm italic">
+            {book.authors.map((author) => `${author} `)}
+          </h5>
         </div>
 
-        <p className="text-[10px]">Reading</p>
+        <p className="text-[10px]">Chapter 2 From 9</p>
       </div>
     </div>
   );
